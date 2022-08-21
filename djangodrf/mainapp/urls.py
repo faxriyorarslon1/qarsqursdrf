d819a9e13a0e6557d4495922693ea9d0cd072b1b
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2022-08-21 22:42:11 +0500

    add gitignore

--- djangodrf/mainapp/urls.py
+++ djangodrf/mainapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ListCreateAboutUs, AboutRetrieveAPIView, AboutUsDelete
+from .views import AboutUsDelete, ListCreateAboutUs, AboutRetrieveAPIView
 
 urlpatterns = [ 
     path('', ListCreateAboutUs.as_view()),
